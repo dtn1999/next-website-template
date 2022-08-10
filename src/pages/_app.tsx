cc7c42cfb5f8a5945441e39314bbcdd5df540b44
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-10 22:32:02 +0200

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "@app/styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -7,9 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="xs:px-20 flex min-h-screen w-full justify-center bg-slate-100 pt-8 antialiased sm:px-10 md:pt-28">
       <div className="w-full max-w-4xl">
-        <ErrorBoundary fallback={<div> error happen </div>}>
-          <Component {...pageProps} />
-        </ErrorBoundary>
+        <Component {...pageProps} />
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import type { NextPage } from "next";
-import ComponentSwitcher from "../components";
+import ComponentSwitcher from "@app/components";
 
 const HomePageComponent: NextPage = () => {
   const isLoading = true;
   return (
     <div className="flex flex-col items-center justify-start bg-white py-[5rem] px-8 text-lg">
-      <h1 className="mb-8 text-3xl text-yellow-600">Welcome to this starter</h1>
+      <h1 className="mb-8 text-3xl text-yellow-600">
+        Welcome the nextjs with Graphcms starter
+      </h1>
       <p className="mb-2 text-lg font-light">
         {" "}
         change the code in the{" "}
@@ -16,6 +18,7 @@ const HomePageComponent: NextPage = () => {
         file to remove the annoying spinner{" "}
       </p>
       {isLoading ? <ComponentSwitcher typename="Spinner" /> : <div></div>}
+      {}
     </div>
   );
 };
